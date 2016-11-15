ad8f8a872a4934e3508cd6bb60f5921cbd5addfc
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-11-14 19:53:38 -0800

    change the leave the first letter alone if the first letter is s.

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -3,9 +3,19 @@ class String
     leet_string = self
     speak_array = []
     leet_array = leet_string.split(//)
+    count = 0
     leet_array.each() do |letter|
-      if letter.eql?("e")
+      count += 1
+      if (letter == "s") && (count == 1)
+        speak_array.push(letter)
+      elsif letter.eql?("e")
         speak_array.push("3")
+      elsif letter.eql?("I")
+        speak_array.push("1")
+      elsif letter.eql?("o")
+        speak_array.push("0")
+      elsif (letter == "s")
+        speak_array.push("z")
       else
         speak_array.push(letter)
       end
